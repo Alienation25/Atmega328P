75b2d884f636acdc34030389244e89d48ee78cb5
Alienation25 <yarisvet2000@gmail.com>
2020-09-22 04:18:38 +0300

    add lux and Pressure

--- BME280_I2C/src/main.cpp
+++ BME280_I2C/src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 
-
-
 #include <Wire.h>
 
 #include <SparkFunBME280.h>
@@ -14,7 +12,7 @@ void setup()
   Serial.begin(9600);
   Serial.println("Example showing alternate I2C addresses");
 
-  Wire.begin();
+  
 
  //The default for the SparkFun Environmental Combo board is 0x77 (jumper open).
   //If you close the jumper it is 0x76
--- LCD_I2C_ENCODER_MENY/src/main.cpp
+++ LCD_I2C_ENCODER_MENY/src/main.cpp
@@ -16,6 +16,9 @@
 #include <Arduino.h>
 #include<Wire.h>
 #include<LiquidCrystal_I2C_OLED.h>
+#include <Max44009.h>
+#include<SparkFunBME280.h>
+
 #include<GyverEncoder.h>
 
 
@@ -29,17 +32,23 @@
 #define maxD 10
 #define minD 1
 
-
-Encoder enc1(CLK, DT, SW);  // энкодер с кнопкой
-
-
 enum Language
 {
  English,
  Russion,
 };
 
+
+Encoder enc1(CLK, DT, SW);  // энкодер с кнопкой
 LiquidCrystal_I2C lcd(0x27,16,2,English);//экран Путь I2C и диагональ
+BME280 sensorPressure(0x76);
+Max44009 sensorLuxA(0x4A);
+Max44009 sensorLuxB(0x4B);
+
+
+
+
+
 
 enum Display_panel
 {
@@ -50,23 +59,42 @@ enum Display_panel
 };
 
 volatile unsigned char numD = 1;//Номер экрана с котором работаем сейчас 
-
-
+float LuxA = 0 ;
+float LuxB = 0 ;
+float Pressure=0;
+char buffer[8];
+String display_text;
 
 void control_Display(unsigned char numD){
   switch (numD)
    {
    case DISPLAY_TEMP://дисплей отвечающий за температуру 
-     lcd.printLCD("Hello","Привет",1,0);  
-    
+     lcd.clear();
+     
+     display_text="";  
      break;
    case DISPLAY_WIND://дисплей для ветра 
+     lcd.clear();
+      //display_text="Pres ";
+      //display_text+=dtostrf(Pressure,3,1,buffer);
+      lcd.print(sensorPressure.readFloatPressure());    
     
-    lcd.printLCD("WIND: WET","Ветер: ветрений",1,0);   
-    
+     display_text="";  
+  
+
+
      break;
    case DISPLAY_LUNIM://lbcgktq 
-     lcd.printLCD("DAY: Time","День: время",1,0);  
+     lcd.clear();
+     display_text="luxA ";
+     display_text+=dtostrf(LuxA,3,1,buffer);
+     display_text+="luxB ";
+     display_text+=dtostrf(LuxB,3,1,buffer);
+      
+      lcd.print(display_text.substring(0,9));  
+      lcd.setCursor(0,1);
+      lcd.print(display_text.substring(9));
+      display_text="";  
   
     break;
    default:
@@ -76,6 +104,7 @@ void control_Display(unsigned char numD){
 
 
 void system_program_encoder(){//функция для отлова движения энкодера   
+   
     enc1.tick(); 
     if(enc1.isTurn())//если произошло движения энкодера в любую сторону
     {
@@ -114,7 +143,7 @@ void setup() {
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(3,0);
+  lcd.setCursor(0,0);
   control_Display(numD);
   enc1.setType(TYPE2);
   
@@ -126,8 +155,14 @@ void setup() {
 
 
 void loop() {
-
    system_program_encoder();
- 
+
+   LuxA=sensorLuxA.getLux();
+   LuxB=sensorLuxB.getLux();
+//   Pressure=sensorPressure.readFloatPressure();
   
+   Serial.print(" PressureA: ");
+  Serial.print(sensorPressure.readFloatPressure(), 0);
+
+
 }
